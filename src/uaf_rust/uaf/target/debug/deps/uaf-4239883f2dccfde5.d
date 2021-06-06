4ca933254d7c60fc7a89f1c26acb6d36cd8d63df
Rain Wang <3045465496@qq.com>
2021-06-06 10:14:18 +0800

    [feat] add cargo project of uaf (final)

---
+++ src/uaf_rust/uaf/target/debug/deps/uaf-4239883f2dccfde5.d
@@ -0,0 +1,5 @@
+/mnt/d/c2rust/C2SafeRust/src/uaf_rust/uaf/target/debug/deps/uaf-4239883f2dccfde5: src/main.rs
+
+/mnt/d/c2rust/C2SafeRust/src/uaf_rust/uaf/target/debug/deps/uaf-4239883f2dccfde5.d: src/main.rs
+
+src/main.rs:
---
+++ src/uaf_rust/uaf/target/debug/uaf.d
@@ -0,0 +1,1 @@
+/mnt/d/c2rust/C2SafeRust/src/uaf_rust/uaf/target/debug/uaf: /mnt/d/c2rust/C2SafeRust/src/uaf_rust/uaf/src/main.rs build.rs
